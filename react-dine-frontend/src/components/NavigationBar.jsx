5662185992da74faff191ae5a3661bde96ab0da0
TyostoKarry <samuritala@hotmail.com>
2023-12-18 21:19:54 +0200

    Added icons for navigationBar links from fontawesome and added hower effect for them

--- react-dine-frontend/src/components/NavigationBar.jsx
+++ react-dine-frontend/src/components/NavigationBar.jsx
@@ -1,18 +1,26 @@
-import { Link } from "react-router-dom";
-
 import "./NavigationBar.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const NavigationBar = () => {
   return (
-    <div className="Navigation-bar__container">
-      <Link className="navBar__button" to={"/"}>
-        Menu
-      </Link>
-      <img src={"./src/assets/TyostoDinerTextLogo.png"} />
-      <Link className="navBar__button" to={"/cart"}>
-        Cart
-      </Link>
-    </div>
+    <>
+      <div className="Navigation-bar__container">
+        <Link className="navBar__button" to={"/"}>
+          <FontAwesomeIcon icon={faUtensils} />
+          <span className="navBar__button-text">Menu</span>
+        </Link>
+        <Link className="navBar__button" to={"/cart"}>
+          <FontAwesomeIcon icon={faCartShopping} />
+          <span className="navBar__button-text">Cart</span>
+        </Link>
+      </div>
+      <img
+        src={"./src/assets/TyostoDinerTextLogo.png"}
+        className="navBar__img"
+      />
+    </>
   );
 };
 
